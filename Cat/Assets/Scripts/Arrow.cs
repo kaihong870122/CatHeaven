47f1b4b1111a989a0bbb979dc7c59c3123801a65
kaihong870122 <57181273+kaihong870122@users.noreply.github.com>
2020-02-21 21:38:55 +0800

    玩家會動
    接下要做:
    1.玩家方向動畫

---
+++ Cat/Assets/Scripts/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+public class Arrow : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(-1f, 0, 0);
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+
+
+    public void Right(int v)
+    {
+        gameObject.transform.position += new Vector3(v * 0.01f, 0, 0);
+    }
+    public void UP(int h)
+    {
+        gameObject.transform.position += new Vector3(0, h * 0.01f, 0);
+    }
+
+    
+
+}
+
